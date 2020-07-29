5a7c5822e6058e49f7ed9f67bf348bed6e6aef81
LianBai <841774407@qq.com>
2020-07-29 17:22:59 +0800

    增加LBUtil和LBEditor中的EditorPlatformModule

---
+++ Assets/LBFramework/LBUtils/0.LBEditor/Editor/EditorModulizationPlatform/IEditorPlatformModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LBFramework.LBEditor
+{
+    public interface IEditorPlatformModule
+    {
+        /// <summary>
+        /// 渲染 IMGUI
+        /// </summary>
+        void OnGUI();
+    }
+}
+
