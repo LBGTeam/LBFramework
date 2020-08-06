284c873ca37f36b90c3ae0217fba7254aec9bf33
LianBai <841774407@qq.com>
2020-08-06 20:19:18 +0800

    添加事件系统

---
+++ Assets/LBFramework/LBUtils/4.LBEventSystem/LBEventComArg.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+namespace LBFramework.LBUtils
+{
+    public class LBEventComArg
+    {
+        #region 获取多参数类型
+
+        public bool TryGet<T>(out T t)
+        {
+            LBEventOneArg<T> arg = this as LBEventOneArg<T>;
+            arg.Get(out t);
+            return true;
+        }
+        public bool TryGet<T,U>(out T t,out U u)
+        {
+            LBEventTwoArg<T,U> arg = this as LBEventTwoArg<T,U>;
+            arg.Get(out t,out u);
+            return true;
+        }
+        public bool TryGet<T,U,K>(out T t,out U u,out K k)
+        {
+            LBEventThreeArg<T,U,K> arg = this as LBEventThreeArg<T,U,K>;
+            arg.Get(out t,out u,out k);
+            return true;
+        }
+        public bool TryGet<T,U,K,I>(out T t,out U u,out K k,out I i)
+        {
+            LBEventFourArg<T,U,K,I> arg = this as LBEventFourArg<T,U,K,I>;
+            arg.Get(out t,out u,out k,out i);
+            return true;
+        }
+        public bool TryGet<T,U,K,I,W>(out T t,out U u,out K k,out I i,out W w)
+        {
+            LBEventFiveArg<T,U,K,I,W> arg = this as LBEventFiveArg<T,U,K,I,W>;
+            arg.Get(out t,out u,out k,out i,out w);
+            return true;
+        }
+
+        #endregion
+    }
+    #region  多参数数据类
+    public class LBEventOneArg<T> : LBEventComArg
+    {
+        private T m_t;
+        public void Set(T t) { m_t = t; }
+        public void Get(out T t) { t = m_t; }
+    }
+    public class LBEventTwoArg<T,U> : LBEventComArg
+    {
+        private T m_t; private U m_u;
+        public void Set(T t,U u) { m_t = t; m_u = u; }
+        public void Get(out T t,out U u) { t = m_t; u = m_u; }
+    }
+    public class LBEventThreeArg<T,U,K> : LBEventComArg
+    {
+        private T m_t; private U m_u; private K m_k;
+        public void Set(T t,U u,K k) { m_t = t; m_u = u; m_k = k; }
+        public void Get(out T t,out U u,out K k) { t = m_t; u = m_u; k = m_k; }
+    }
+    public class LBEventFourArg<T,U,K,I> : LBEventComArg
+    {
+        private T m_t; private U m_u; private K m_k; private I m_i;
+        public void Set(T t,U u,K k,I i) { m_t = t; m_u = u; m_k = k; m_i = i; }
+        public void Get(out T t,out U u,out K k,out I i) { t = m_t; u = m_u; k = m_k; i = m_i; }
+    }
+    public class LBEventFiveArg<T,U,K,I,W> : LBEventComArg
+    {
+        private T m_t; private U m_u; private K m_k; private I m_i; private W m_w;
+        public void Set(T t,U u,K k,I i,W w) { m_t = t; m_u = u; m_k = k; m_i = i; m_w = w; }
+        public void Get(out T t,out U u,out K k,out I i,out W w) { t = m_t; u = m_u; k = m_k; i = m_i; w = m_w; }
+    }
+    #endregion
+    
+}
---
+++ Assets/LBFramework/LBUtils/4.LBEventSystem/LBEventDispatcher.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace LBFramework.LBUtils
+{
+    //自定义事件委托类型
+    public delegate void CustomEventHandler(LBEventComArg arg = null);
+    
+    public class LBEventDispatcher
+    {
+        private readonly Dictionary<int, HashSet<ILBEventHandler>> id2HandlerDict = new Dictionary<int, HashSet<ILBEventHandler>>();
+        private readonly Dictionary<int, CustomEventHandler> id2DeleDict = new Dictionary<int, CustomEventHandler>();
+
+        public void AddListener(int eventId, ILBEventHandler handler)
+        {
+            if (!id2HandlerDict.TryGetValue(eventId, out HashSet<ILBEventHandler> handlerSet))
+                handlerSet = id2HandlerDict[eventId] = new HashSet<ILBEventHandler>();
+            handlerSet.Add(handler);
+        }
+        
+        public void AddListener(int eventId, CustomEventHandler eventHandleDele)
+        {
+            if (id2DeleDict.TryGetValue(eventId, out CustomEventHandler eventDele))
+            {
+                Delegate[] delegates = eventHandleDele.GetInvocationList();
+                if (Array.IndexOf(delegates, eventHandleDele) == -1)
+                    eventDele += eventHandleDele;
+            }
+            else
+                id2DeleDict.Add(eventId, eventHandleDele);
+        }
+
+        public void SendEvent(int eventId)
+        {
+            TiggerEvent(eventId,null);        //当函数参数不需要赋值的时候就赋值一个NULL,减少GC
+        }
+
+        public void SendEvent<T>(int eventId, T arg1)
+        {
+            //临时闯将将来可以换成对象池
+            LBEventOneArg<T> eventArg = new LBEventOneArg<T>();
+            eventArg.Set(arg1);
+            TiggerEvent(eventId,eventArg);
+            //如果用对象池，在这里就可以回收
+        }
+        public void SendEvent<T,U>(int eventId, T arg1,U arg2)
+        {
+            //临时闯将将来可以换成对象池
+            LBEventTwoArg<T,U> eventArg = new LBEventTwoArg<T,U>();
+            eventArg.Set(arg1,arg2);
+            TiggerEvent(eventId,eventArg);
+            //如果用对象池，在这里就可以回收
+        }
+        public void SendEvent<T,U,K>(int eventId, T arg1,U arg2,K arg3)
+        {
+            //临时闯将将来可以换成对象池
+            LBEventThreeArg<T,U,K> eventArg = new LBEventThreeArg<T,U,K>();
+            eventArg.Set(arg1,arg2,arg3);
+            TiggerEvent(eventId,eventArg);
+            //如果用对象池，在这里就可以回收
+        }
+        public void SendEvent<T,U,K,I>(int eventId, T arg1,U arg2,K arg3,I arg4)
+        {
+            //临时闯将将来可以换成对象池
+            LBEventFourArg<T,U,K,I> eventArg = new LBEventFourArg<T,U,K,I>();
+            eventArg.Set(arg1,arg2,arg3,arg4);
+            TiggerEvent(eventId,eventArg);
+            //如果用对象池，在这里就可以回收
+        }
+
+
+        private void TiggerEvent(int eventId, LBEventComArg arg)
+        {
+            if (id2HandlerDict.TryGetValue(eventId, out HashSet<ILBEventHandler> handlerSet))
+            {
+                foreach (var handler in handlerSet)
+                {
+                    handler.HandleEvent(eventId, arg);
+                }
+            }
+            if (id2DeleDict.TryGetValue(eventId, out CustomEventHandler oldDele))
+            {
+                oldDele.Invoke(arg);
+            }
+        }
+        
+        public void RemoveListener(int eventId, ILBEventHandler handler)
+        {
+            if (id2HandlerDict.TryGetValue(eventId, out HashSet<ILBEventHandler> handlerSet))
+            {
+                id2HandlerDict[eventId].Remove(handler);
+            }
+        }
+
+        public void RemoveListener(int eventId, CustomEventHandler eventHandlerDele)
+        {
+            if (id2DeleDict.TryGetValue(eventId, out CustomEventHandler oldDele))
+            {
+                oldDele -= eventHandlerDele;
+                if (oldDele == null)
+                    id2DeleDict.Remove(eventId);
+            }
+        }
+        public void Destruct()
+        {
+            id2HandlerDict.Clear();
+            id2DeleDict.Clear();
+        }
+    }
+}
