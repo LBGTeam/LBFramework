51e36dc013bd2aaf82b5bb4d975e5c2120156c7f
LianBai <841774407@qq.com>
2020-08-10 17:43:25 +0800

    增减对象池单元测试中的SimpleObjectPool(简单对象池)的单元测试

---
+++ Assets/LBFramework/LBUnitTest/ObjectPoolTests.cs
@@ -0,0 +1,36 @@
+﻿using LBFramework.LBUtils;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace LBFramework.NUnitTest
+{
+    public class ObjectPoolTests
+    {
+        public class Fish
+        {
+        }
+
+        [Test]
+        public void SimpleObjectPool_Test()
+        {
+            var fishPool = new SimpleObjectPool<Fish>(() => new Fish(), null, 100);
+
+            Assert.AreEqual(fishPool.CurCount, 100);
+
+            var fishOne = fishPool.Allocate();
+
+            Assert.AreEqual(fishPool.CurCount, 99);
+
+            fishPool.Recycle(fishOne);
+
+            Assert.AreEqual(fishPool.CurCount, 100);
+
+            for (var i = 0; i < 10; i++)
+            {
+                fishPool.Allocate();
+            }
+
+            Assert.AreEqual(fishPool.CurCount, 90);
+        }
+    }
+}
